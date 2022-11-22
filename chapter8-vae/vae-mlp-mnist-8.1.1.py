7b4ac38aa8dc752c8eb519c39a86aa52f0767b31
Jonas Gacrama <jonasgacrama@gmail.com>
2022-11-22 18:38:03 +0800

    [master] Removed extraneous tensorflow namespace in VAE

--- chapter8-vae/vae-mlp-mnist-8.1.1.py
+++ chapter8-vae/vae-mlp-mnist-8.1.1.py
@@ -17,12 +17,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow.keras.layers import Lambda, Input, Dense
-from tensorflow.keras.models import Model
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.losses import mse, binary_crossentropy
-from tensorflow.keras.utils import plot_model
-from tensorflow.keras import backend as K
+from keras.layers import Lambda, Input, Dense
+from keras.models import Model
+from keras.datasets import mnist
+from keras.losses import mse, binary_crossentropy
+from keras.utils import plot_model
+from keras import backend as K
 
 import numpy as np
 import matplotlib.pyplot as plt
