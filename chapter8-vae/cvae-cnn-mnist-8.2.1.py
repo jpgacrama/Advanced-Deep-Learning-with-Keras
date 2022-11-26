cea4f0173951a051563411ec809d425c4efabe67
Jonas Gacrama <jonasgacrama@gmail.com>
2022-11-26 18:09:28 +0800

    [master] Removed tensorflow namespace in CVAE

--- chapter8-vae/cvae-cnn-mnist-8.2.1.py
+++ chapter8-vae/cvae-cnn-mnist-8.2.1.py
@@ -16,16 +16,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras.layers import Conv2D, Flatten, Lambda
-from tensorflow.keras.layers import Reshape, Conv2DTranspose
-from tensorflow.keras.layers import concatenate
-from tensorflow.keras.models import Model
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.losses import mse, binary_crossentropy
-from tensorflow.keras.utils import plot_model
-from tensorflow.keras import backend as K
-from tensorflow.keras.utils import to_categorical
+from keras.layers import Dense, Input
+from keras.layers import Conv2D, Flatten, Lambda
+from keras.layers import Reshape, Conv2DTranspose
+from keras.layers import concatenate
+from keras.models import Model
+from keras.datasets import mnist
+from keras.losses import mse, binary_crossentropy
+from keras.utils import plot_model
+from keras import backend as K
+from keras.utils import to_categorical
 
 import numpy as np
 import matplotlib.pyplot as plt
